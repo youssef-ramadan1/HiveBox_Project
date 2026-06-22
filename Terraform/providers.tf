3ed346df523d84c11971829f5c04c71cf6c99d15
Youssef <youssefaboshafei@gmail.com>
2026-06-22 15:12:46 +0300

    version infinit

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "2.38.0"
     }
   }
 }
 
 provider "kubernetes" {
+  # مسار ملف الـ config اللي جينكنز بنسخه للـ runner
   config_path = "../k3s-config.yaml"
+
+  # السطر السحري اللي هيتخطى حماية الـ TLS والشهادات عشان نربط بالـ كونتير الموضعي سليم
+  insecure = true
 }
